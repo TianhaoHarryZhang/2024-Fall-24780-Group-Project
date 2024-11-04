404b0ca4756e8d38042337f53d22c481a67a19d8
TianhaoHarryZhang <tianhao_zhang@hotmail.com>
2024-11-03 22:40:15 -0500

    Added some source/header files

---
+++ Trainer.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Trainer.h" 
+
+
+void Trainer::move(float dx, float dy)
+{
+	x += dx;
+	y += dy;
+}
---
+++ Trainer.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+class Trainer
+{
+private:
+
+	float x, y;
+
+public:
+
+	void move(float dx, float dy);
+};
+
