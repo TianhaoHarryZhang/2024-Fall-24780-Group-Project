2d423160fc20c72b032e870a5dac5014b9c46b44
KaifengHu <kaifengh@andrew.cmu.edu>
2024-11-16 12:00:10 -0500

    Battle scene update

--- BattleScene.cpp
+++ BattleScene.cpp
@@ -24,10 +24,36 @@ void PokemonUI::renderTextBox(const std::string& message, int positionX, int pos
 int main() {
     while (true) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
         int terminate = 0;
+        int lb, mb, rb, mx, my;
+        int exit_x, exit_y, exit_h, exit_w;
+        int attack_x, attack_y, attack_h, attack_w;
+        int bag_x, bag_y, bag_h, bag_w;
 
+        FsGetMouseState(lb, mb, rb, mx, my);
 
+        // Load battle scene here
+        
 
+        // Check if any button is pressed
+        if (lb == 1) {
+            if (mx > exit_x && mx < exit_x + exit_w && my > exit_y && my < exit_y + exit_h) {
+                terminate = 1;
+            }
+            else if (mx > attack_x && mx < attack_x + attack_w && my > attack_y && my < attack_y + attack_h) {
+                // Put attack animation here
+
+            }
+            else if (mx > bag_x && mx < bag_x + bag_w && my > bag_y && my < bag_y + bag_h) {
+                // Put backpack scene here
+
+            }
+        }
+
+        if (terminate == 1) {
+            break;
+        }
 
         FsSwapBuffers();
     }
