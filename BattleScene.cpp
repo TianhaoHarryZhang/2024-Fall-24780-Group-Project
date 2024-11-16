0c050ce916c2096780028bad323a11722dbd384a
KaifengHu <kaifengh@andrew.cmu.edu>
2024-11-16 17:53:23 -0500

    battle scene update

--- BattleScene.cpp
+++ BattleScene.cpp
@@ -25,15 +25,21 @@ void PokemonUI::renderTextBox(const std::string& message, int positionX, int pos
 int main() {
     Pokemon pokemon1;
     Pokemon pokemon2;
+    Pokemon NPCpokemon;
     PokemonUI UI;
 
+    Pokemon* currentPokemon = &pokemon1; // The pokeemon that is currently selected, defalut is pokemon 1
     bool terminate = false;
-    int lb, mb, rb, mx, my;
-    int exit_x, exit_y, exit_h, exit_w;
-    int attack1_x, attack1_y, attack2_x, attack2_y, attack_h, attack_w;
-    int bag_x, bag_y, bag_h, bag_w;
+    int lb, mb, rb, mx, my; // Store the position and state of the mouse
+    int exit_x, exit_y, exit_h, exit_w; // Rendering attribute for the exit button
+    int attack1_x, attack1_y, attack2_x, attack2_y, attack_h, attack_w; // Rendering attribute for the attack button
+    int bag_x, bag_y, bag_h, bag_w; // Rendering attribute for the backpack button
+    int hp_player_x, hp_player_y, hp_player_h, hp_player_w; // Player pokemon's hp bar positon
+    int hp_NPC_x, hp_NPC_y, hp_NPC_h, hp_NPC_w; // NPC pokemon's hp bar positon
+    int playerPokemon_x, playerPokemon_y, playerPokemon_scale, playerPokemon_direction; // Player pokemon's rendering attribute
+    int NPCpokemon_x, NPCpokemon_y, NPCpokemon_scale, NPCpokemon_direction; // NPC pokemon's rendering attribute
     bool playerRound = true; // Player always go first
-    int currentPokemon = 1; // Default Pokemon is the first pokemon
+    int pokemonSelect = 1; // Default Pokemon is the first pokemon
     
 
     while (true) {
@@ -43,30 +49,34 @@ int main() {
         FsGetMouseState(lb, mb, rb, mx, my);
 
         // Load battle scene here
-        if (currentPokemon == 1) {
-            UI.renderHPBar(pokemon1.hp);
-        }
-        else {
-            UI.renderHPBar(pokemon2.hp);
-        }
+        // Render pokeemon and their hp bar
         
+
+        UI.renderHPBar(currentPokemon->hp, currentPokemon->maxHp, hp_player_x, hp_player_y, hp_player_h, hp_player_w);
+        currentPokemon->render(playerPokemon_x, playerPokemon_y, playerPokemon_scale, playerPokemon_direction);
+        UI.renderHPBar(NPCpokemon.hp, NPCpokemon.maxHp, hp_NPC_x, hp_NPC_y, hp_NPC_h, hp_NPC_w)
+        NPCpokemon.render(NPCpokemon_x, NPCpokemon_y, NPCpokemon_scale, NPCpokemon_direction);
+
 
 
         // Check if it is player's round
         if (playerRound == true){
             // Check if any button is pressed
             if (lb == 1) {
+                // Exit button pressed
                 if (mx > exit_x && mx < exit_x + exit_w && my > exit_y && my < exit_y + exit_h) {
                     terminate = true;
                 }
-                // First skill
+                // First skill pressed
                 else if (mx > attack1_x && mx < attack1_x + attack_w && my > attack1_y && my < attack1_y + attack_h) {
-                    playerRound = !playerRound;
+                    playerRound = !playerRound; // Flip the round to NPC
+
                 }
-                // Second skill
+                // Second skill pressed
                 else if (mx > attack2_x && mx < attack2_x + attack_w && my > attack2_y && my < attack2_y + attack_h) {
-                    playerRound = !playerRound;
+                    playerRound = !playerRound; // Flip the round to NPC
             }
+                // Backpack pressed
             else if (mx > bag_x && mx < bag_x + bag_w && my > bag_y && my < bag_y + bag_h) {
                 // Put backpack scene here
 
@@ -80,6 +90,14 @@ int main() {
 
         if (terminate == true) {
             break;
+        }
+
+        // Check the position of the 
+        if (pokemonSelect == 1) {
+            currentPokemon = &pokemon1;
+        }
+        else {
+            currentPokemon = &pokemon2;
         }
 
          FsSwapBuffers();
