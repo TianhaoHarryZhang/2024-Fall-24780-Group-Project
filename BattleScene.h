a72ce2c5bf71948fbdc3775e2df057be315d8f22
Rachell Soojin Lee <50713036+rachell7@users.noreply.github.com>
2024-11-24 19:07:52 -0500

    Scene Transition from Battle to Medicine (#10)
    * Try w/ new font func
    
    * Scene Transition

--- BattleScene.h
+++ BattleScene.h
@@ -1,9 +1,10 @@
 #ifndef BATTLE_SCENE_H
 #define BATTLE_SCENE_H
-
+#include "Scene.h"
 #include <string>
 
-class PokemonUI {
+class PokemonUI
+{
 public:
     // Renders the HP bar for a Pokémon
     void renderHPBar(float hp, float maxHp, int positionX, int positionY, int width, int height);
@@ -12,13 +13,13 @@ public:
     void renderOptions(int positionX, int positionY, std::string filename);
 
     // Renders a text box to display messages, e.g., "Charizard used Fire Blast!"
-    void renderTextBox(const std::string& message, int positionX, int positionY, int width, int height);
+    void renderTextBox(const std::string &message, int positionX, int positionY, int width, int height);
 
     void renderName(std::string name, int positionX, int positionY);
 
     void renderBK();
-};
 
-int battle();
+    int battle(Scene_State *scene_state);
+};
 
 #endif
