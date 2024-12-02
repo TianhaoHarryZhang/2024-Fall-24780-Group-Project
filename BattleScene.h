0cdc7533242199bbd8ccc5538e8c22c38198bdeb
kaifenghu99 <150211536+kaifenghu99@users.noreply.github.com>
2024-12-01 21:01:56 -0500

    Update BattleScene.h
    Update to include new functions

--- BattleScene.h
+++ BattleScene.h
@@ -11,13 +11,18 @@ public:
     // Renders the options for a Pokémon battle, like "Fight," "Bag," "Run," etc.
     void renderOptions(int positionX, int positionY, std::string filename);
 
+    // Renders the two buttons for skill
+    void renderSkillSelection(Pokemon* pokemon, int positionX1, int positionY1, int positionX2, int positionY2);
+
     // Renders a text box to display messages, e.g., "Charizard used Fire Blast!"
     void renderTextBox(const std::string& message, int positionX, int positionY, int width, int height);
 
     void renderName(std::string name, int positionX, int positionY);
 
     void renderBK();
-};
+
+    void PokemonUI::performAttack(Pokemon* attacker, Pokemon* defender, int skillNumber, int attacker_x, int attacker_y, float attacker_scale, int attacker_direction,int* animation_counter, bool* attacker_in_animation,int* skill_animation_counter, bool* attacker_in_skill_animation);
+    };
 
 int battle();
 
